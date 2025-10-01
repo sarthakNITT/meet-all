6671864496c8c154b722a4341644a19206ecb7f3
sarthakKarode <sarthakkarode@gmail.com>
2025-10-01 18:48:56 +0530

    added ws server open event

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!");
+let checkJoinReq = false;
+
+Bun.serve({
+    port: 8080,
+    fetch(req, server) {
+        if (server.upgrade(req)) {
+            return; // do not return a Response
+        }
+          return new Response("Upgrade failed", { status: 500 });
+    },
+    websocket: {
+        open(ws){
+            let socketId: string = "";
+            let ch = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()";
+            for(let i=0;i<8;i++){
+                socketId += ch.charAt(Math.floor(Math.random() * ch.length));
+            }
+            const sendMessage = JSON.stringify({
+                "type": "connection",
+                "value": "successfull",
+                "socketId": `${socketId}`
+            })
+            ws.send(sendMessage);
+            setTimeout(() => {
+                if(!checkJoinReq){
+                    ws.close();
+                    return;
+                }
+            }, 10000);
+        },
+        message(ws, message: Buffer<ArrayBufferLike> | string){
+            const msg = typeof message === "string" ? JSON.parse(message) : null;
+            if(msg.type === "join"){
+
+            }else if(msg.type === "create"){
+                
+            }
+        }
+    }
+})
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useRef } from "react";
 import { getConnectedDevices } from "./utils/helperFunctions/getConnectedDevices";
 import { playVideoFromCamera } from "./utils/helperFunctions/playVideoFromCamera";
 import { updateCameraList } from "./utils/helperFunctions/updateCameraList";
 
 export default function Home () {
+  const socketRef = useRef<WebSocket | null>(null);
   const openMediaDevices = async (constraints: MediaStreamConstraints) => {
     return await navigator.mediaDevices.getUserMedia(constraints);
   }
@@ -27,10 +29,22 @@ export default function Home () {
     updateCameraList(newCameraList);
   });
 
+  async function handleConnection () {
+    const socket = new WebSocket("ws://localhost:8080");
+    socketRef.current = socket;
+    socketRef.current.onmessage = event => {
+      const msg: any = typeof event.data === "string" ? JSON.stringify(event.data) : null;
+      if(msg.type === "connection"){
+        console.log(msg.socketId);
+      }
+    }
+  }
+
   return (
     <div>
       <video id="localVideo" autoPlay playsInline controls={false}/>
       <button onClick={playVideoFromCamera}>Play Video from Camera</button>
+      <button onClick={handleConnection}>Connect to ws server</button>
     </div>
   )
 }
